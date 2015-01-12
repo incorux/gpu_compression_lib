c5b56a64444b7840a71a99e6db5f3b07f9168f09
Piotr Przymus <piotr.przymus@gmail.com>
2015-01-12 10:06:59 +0100

    macros fix

--- compression/macros.cuh
+++ compression/macros.cuh
@@ -1,14 +1,31 @@
 #ifndef macros
 #define macros 0
 
+#if __CUDA_ARCH__ > 120  // This improves performance 
 
-// Te makra powinny dzialac prawidlowo nie zaleznie od implementacji
+__device__ __inline__ unsigned int GETNPBITS(
+  int source,
+  unsigned int num_bits,
+  unsigned int bit_start)
+ {
+  unsigned int bits;
+  asm("bfe.u32 %0, %1, %2, %3;" : "=r"(bits) : "r"((unsigned int) source), "r"(bit_start), "r"(num_bits));
+  return bits;
+ }
+#define GETNBITS(X,N) GETNPBITS(X,N,0)
 
+#else
+
+#define GETNPBITS(a, n, p) GETNBITS((a>>p), (n)) 
+#define GETNBITS(a,n)  ((a) & NBITSTOMASK(n))
+
+#endif
+
+
+// This should work independently from _CUDA_ARCH__ number
 #define SGN(a) (int)((unsigned int)((int)a) >> (sizeof(int) * CHAR_BIT - 1))
 #define NBITSTOMASK(n) ((1<<(n)) - 1)
-#define GETNBITS(a,n)  ((a) & NBITSTOMASK(n))
 #define GETNSGNBITS(a,n,b) ((SGN(a) << (n-1)) | GETNBITS(((a)>>(b-n)), (n-1))) 
-#define GETNPBITS(a, n, p) GETNBITS((a>>p), (n)) 
 #define RECONSTRUCT(a1, n1, p1, a2, n2, p2) GETNPBITS(a1, n1, p1) << (n2) | GETNPBITS(a2, n2, p2)
 
 #define fillto8(c) (((c + 8 - 1) / 8) * 8)
