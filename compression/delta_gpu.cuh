af7b026e3bd5e372969107a10e75d6fb039f703f
Piotr Przymus <piotr.przymus@gmail.com>
2015-10-05 12:15:54 +0200

    Delta start

--- compression/delta_gpu.cuh
+++ compression/delta_gpu.cuh
@@ -1,77 +1,7 @@
 #ifndef DELTA_CUH_KKZZHX97
 #define DELTA_CUH_KKZZHX97
 
-template <typename T>
-__global__ void delta_compress_gpu (T *data, T *compressed_data, T *spoints, unsigned long length, unsigned long spoints_length) 
-{
-    int tid = ((blockIdx.x * blockDim.x) + threadIdx.x);
-    int laneId = threadIdx.x & 0x1f;
-    int warpId = tid / 32; //TODO: fix
+template <typename T> __global__ void delta_compress_gpu (T *data, T *compressed_data, T *spoints, unsigned long length, unsigned long spoints_length);
 
-    int value1=0;
-    int value2=0;
-    int zeroLaneValue=0;
-
-    char neighborId = laneId - 1;
-
-    //TODO: add if data index in range
-    value1 = data[tid];
-    zeroLaneValue = value1;
-
-    if (laneId == 0)  {
-        neighborId = 31; 
-        spoints[warpId] = value1;
-    }
-
-    int ret = 0;
-
-    for (int i = 1;  i < 32; ++ i)
-    {
-        value2 = __shfl( value1, neighborId, 32); // Get previous value from neighborId
-
-        if (laneId == 0)
-        {
-            printf("Thread %d final value = %d zeroLaneValue %d, value1 %d\n", threadIdx.x, zeroLaneValue - value1, zeroLaneValue, value1);
-            ret = zeroLaneValue - value1;
-            zeroLaneValue = value2;
-        } else {
-            ret = value2 - value1;
-            printf("Thread %d final value = %d\n", threadIdx.x, value2 - value1);
-        }
-
-        value1 = data[tid + i * 32]; 
-
-        if (ret != 1)
-        {
-            printf("Error %d ret %d\n", threadIdx.x, ret);
-        }
-    }
-}
-
-template <typename T>
-__global__ void delta_decompress_gpu (T *compressed_data, T *spoints, T *data, unsigned long length, unsigned int spoints_length, int width=32)
-{
-    int id = ((blockIdx.x * blockDim.x) + threadIdx.x);
-    int lane_id = id % warpSize;
-    int warpId = threadIdx.x / 32; //TODO: fix
-
-    T value = data[id] + spoints[warpId];
-    int laneZeroValue=0;
-
-    for (int j = 0;  j < 3; ++ j)
-    {
-        // Now accumulate in log2(32) steps
-        for(int i=1; i <= width; i *= 2) 
-        {
-            int n = __shfl_up(value, i);
-            if(lane_id >= i) value += n;
-        }
-        
-        printf("Thread %d final value = %d\n", threadIdx.x, value);
-        laneZeroValue = __shfl(value, 31);
-        value = data[id];
-
-        if (lane_id == 0) value=laneZeroValue + value;
-    }
-}
+template <typename T> __global__ void delta_decompress_gpu (T *compressed_data, T *spoints, T *data, unsigned long length, unsigned int spoints_length, int width=32);
 #endif /* end of include guard: DELTA_CUH_KKZZHX97 */
