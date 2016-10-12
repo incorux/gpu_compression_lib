e17181ff861d499cd22e3b11806df339d21e56a2
Piotr Przymus <piotr.przymus@gmail.com>
2016-10-12 11:22:36 +0200

    l data tools update

--- real_data_benchmarks/binary_format_reader.c
+++ real_data_benchmarks/binary_format_reader.c
@@ -7,13 +7,13 @@
 #include <sys/mman.h>
 #include <string.h>
 
- #define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
+#define max(a,b) \
+    ({ __typeof__ (a) _a = (a); \
+     __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
 int compare_binary_file(const void *a, const void *b) {
-  return -(*(int *)a - *(int *)b);
+    return -(*(int *)a - *(int *)b);
 }
 
 unsigned int BITLEN(unsigned int word) 
@@ -21,21 +21,21 @@ unsigned int BITLEN(unsigned int word)
     unsigned int ret=1; 
     if(word == 0) return 0;
     while (word >>= 1) 
-      ret++;
-   return ret > 64 ? 0 : ret;
+        ret++;
+    return ret > 64 ? 0 : ret;
 }
 
 int main(int argc, char *argv[])
 {
-    int i;
+    unsigned long i;
     int fd;
-    unsigned int *map;  /* mmapped array of int's */
+    unsigned int *map;  
     if (argc !=2) exit(0);
 
     struct stat st;
     stat(argv[1], &st);
     unsigned long fsize = st.st_size;
-    printf("File size %d Number of ints %d\n", fsize, fsize / 4);
+    printf("File size %lu Number of ints %lu\n", fsize, fsize / 4);
 
     if ((fd = open(argv[1], O_RDONLY))== -1) {
         perror("Error opening file for reading");
@@ -49,48 +49,63 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    /* Read the file int-by-int from the mmap
-    */
-    unsigned int histo[33];
+    int min = map[0], max = map[0];
+    unsigned long histo[33];
     printf("FL\n");
     for (i = 0; i < 33; ++i)  histo[i]=0; 
-    for (i = 0; i <= fsize / 4; ++i)  histo[BITLEN(map[i])]++; 
-    for (i = 0; i < 33; ++i)  printf(" [%d:%u] ",i, histo[i]); 
+    for (i = 0; i < fsize / 4; ++i)  {
+        histo[BITLEN(map[i])]++; 
+        if(min > map[i]) min = map[i];
+        if(max < map[i]) max = map[i];
+    }
+    for (i = 0; i < 33; ++i)  printf(" %lu:%lu, ",i, histo[i]); 
+    printf("\nmin %d max %d\n", min, max);
 
-    int *dest = malloc(fsize);
-    memcpy(dest, map, fsize);
-
-    qsort((void *)dest, fsize/sizeof(int), sizeof(int), &compare_binary_file);
+    printf("\nFOR-FL\n");
+    for (i = 0; i < 33; ++i)  histo[i]=0; 
+    for (i = 0; i < fsize / 4; ++i)  {
+        histo[BITLEN(map[i]-min)]++; 
+    }
+    for (i = 0; i < 33; ++i)  printf(" %lu:%lu, ",i, histo[i]); 
 
     printf("\nDELTA_FL\n");
+    min = map[0] - map[1]; max = map[0]-map[1];
     for (i = 0; i < 33; ++i)  histo[i]=0; 
-    for (i = 1; i <= fsize / 4; ++i)  histo[BITLEN(dest[i-1] - dest[i])]++; 
-    for (i = 0; i < 33; ++i)  printf(" [%d:%u] ",i, histo[i]); 
+    for (i = 1; i < fsize / 4; ++i) {
+        histo[BITLEN(map[i-1] - map[i])]++; 
+        if(min > map[i-1] - map[i]) min = map[i-1] - map[i];
+        if(max < map[i-1] - map[i]) max = map[i-1] - map[i];
+    } 
+    for (i = 0; i < 33; ++i)  printf(" %lu:%lu, ",i, histo[i]); 
+
+    printf("\nDELTA_FOR_FL\n");
+    for (i = 0; i < 33; ++i)  histo[i]=0; 
+    for (i = 1; i < fsize / 4; ++i)  histo[BITLEN(map[i-1] - map[i] - min )]++; 
+    for (i = 0; i < 33; ++i)  printf(" %lu:%lu, ",i, histo[i]); 
 
     printf("\nAAFL\n");
-    unsigned int nblocks = fsize/(4*1024) + 1;
-    int *AAFL_histo = malloc(nblocks * sizeof(int));
+    unsigned long nblocks = fsize/(4*1024) + 1;
     int j;
-
-    for (i = 0; i < nblocks; ++i)  AAFL_histo[i]=0; 
-    for (i = 0; i < nblocks; ++i)  
-        for (j = 0; j < 1024; ++j) 
-            AAFL_histo[i] = max(BITLEN(dest[i*1024 +j]), AAFL_histo[i]);
+    unsigned long cblock_p;
     unsigned long cblock = 0;
+
     for (i = 0; i < nblocks; ++i)  {
-        cblock += AAFL_histo[i] * sizeof(int) * 8; 
+        cblock_p = 0;
+        for (j = 0; j < 1024; ++j) 
+            cblock_p = max(BITLEN(map[i*1024 +j]), cblock_p);
+        cblock += cblock_p * sizeof(int) * 8; 
     }
     printf("Comp r %f \n", (double)fsize / (cblock + (nblocks) *(sizeof(int) +sizeof(char))));
 
     printf("\nDELTA_AAFL\n");
 
     cblock = 0;
-    for (i = 0; i < nblocks; ++i)  AAFL_histo[i]=0; 
-    for (i = 0; i < nblocks; ++i)  
-        for (j = 1; j < 1024; ++j) {
-            AAFL_histo[i] = max(BITLEN(dest[i*1024 + j -1 ] - dest[i*1024 + j]), AAFL_histo[i]);
-        }
-    for (i = 0; i < nblocks; ++i)  cblock += AAFL_histo[i] * sizeof(int) * 8; 
+    for (i = 0; i < nblocks; ++i)  {
+        cblock_p = 0;
+        for (j = 1; j < 1024; ++j) 
+            cblock_p = max(BITLEN(map[i*1024 + j -1 ] - map[i*1024 + j]), cblock_p);
+        cblock += cblock_p * sizeof(int) * 8; 
+    }
     printf("Comp r %f \n", (double)fsize / (cblock + nblocks * ( sizeof(int) + sizeof(int) + sizeof(char))));
 
     if (munmap(map, fsize) == -1) {
--- real_data_benchmarks/tpc-h-oracle-format-converter.py
+++ real_data_benchmarks/tpc-h-oracle-format-converter.py
@@ -5,28 +5,53 @@ import struct
 import datetime
 import time
 
+import concurrent.futures
 
-if __name__ == "__main__":
+# 0, L_ORDERKEY identifier Foreign Key to O_ORDERKEY
+# 1, L_PARTKEY identifier Foreign key to P_PARTKEY, first part of the compound Foreign Key to (PS_PARTKEY, PS_SUPPKEY) with L_SUPPKEY
+# 2, L_SUPPKEY Identifier Foreign key to S_SUPPKEY, second part of the compound Foreign Key to (PS_PARTKEY, TPC Benchmark TM H Standard Specification Revision 2.17.1 Page 16PS_SUPPKEY) with L_PARTKEY
+# 3, L_LINENUMBER integer
+# 4, L_QUANTITY decimal
+# 5, L_EXTENDEDPRICE decimal
+# 6, L_DISCOUNT decimal
+# 7, L_TAX decimal
+# 8, L_RETURNFLAG fixed text, size 1
+# 9, L_LINESTATUS fixed text, size 1
+# 10, L_SHIPDATE date
+# 11, L_COMMITDATE date
+# 12, L_RECEIPTDATE date
+# 13, L_SHIPINSTRUCT fixed text, size 25
+# 14, L_SHIPMODE fixed text, size 10
+# 15, L_COMMENT variable text size 44
 
-    fin = "lineitem.tbl"
-    f_out = { 0: 'ORDERKEY', 3: 'LINENUMBER', 11: 'COMMITDATE', 5: 'EXTENDEDPRICE'}
+fin = "lineitem.tbl"
+f_out = { 4: 'QUANTITY', 1: 'PARTKEY', 10: 'SHIPDATE', 6: 'DISCOUNT', 0: 'ORDERKEY', 3: 'LINENUMBER', 11: 'COMMITDATE', 5: 'EXTENDEDPRICE'}
+columns_ids = f_out.keys()
 
+
+def extrac_to_binary(output_id):
     f_converter = { 
             0: lambda x: int(x), 
+            1: lambda x: int(x), 
             3: lambda x: int(x), 
+            10: lambda x: int(time.mktime(datetime.datetime.strptime(x, "%Y-%m-%d").timetuple())),
             11: lambda x: int(time.mktime(datetime.datetime.strptime(x, "%Y-%m-%d").timetuple())),
-            5: lambda x: int(float(x) * 100) 
-    }
-    fds = {}
-    for k in f_out: fds[k] = open(f_out[k] + ".bin", "wb")
-
+            5: lambda x: int(float(x) * 100),
+            6: lambda x: int(float(x) * 100),
+            4: lambda x: int(float(x) * 100),
+            }
+    fd_out = open(f_out[output_id] + ".bin", "wb")
     io.BufferedIOBase()
-
+    func = f_converter[output_id]
     with open(fin) as f:
         data = csv.reader(f, delimiter="|")
         for record in data:
-            for col in f_out:
-                fds[col].write(struct.pack('=i', f_converter[col](record[col])))
+            fd_out.write(struct.pack('=i', func(record[output_id])))
 
-    for k in fds:
-        fds[k].close()
+    fd_out.close()
+    return output_id
+
+if __name__ == "__main__":
+    with concurrent.futures.ProcessPoolExecutor(len(columns_ids)) as executor:
+        for done in executor.map(extrac_to_binary, columns_ids):
+            print 'Done file %s' % (f_out[done],)
